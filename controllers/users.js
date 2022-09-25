1d5d29fb474aa555bb9ef0226eb4e88b5e6ab8cb
JhosepIslam <jhosepchai@gmail.com>
2022-09-25 10:12:26 -0600

    ✨ parsable json from express  conf

--- controllers/users.js
+++ controllers/users.js
@@ -7,16 +7,22 @@
   }
 
   const postUsers = (req, res) => {
+
+    const {name, age} = req.body;
+
     res.json({
       ok: true,
       msg: "postUsers API",
+      data: {name, age}
     });
   }
 
   const putUsers = (req, res) => {
+    const {id} = req.params;
     res.json({
       ok: true,
       msg: "putUsers API",
+      data: id
     });
   }
 
